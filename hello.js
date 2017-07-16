88830a0247dd4617664853573b8ca6f7b1f33c94
sherwinJay <sherwinsuva@gmail.com>
2017-07-17 04:54:15 +0800

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	//call randomQuote function
+	randomQuote();
+	//load the randomQuote function everytime  .button is click
+	/**$(".button").on("click", function(e){
+		e.preventDefault();
+	  $(this).load(randomQuote());
+	     //$.getJSON("http://quotes.stormconsultancy.co.uk/random.json").done(randomQuote);
+	});**/
+	
+	function randomQuote(){
+		$("form").on("keyup", "input", function(){
+			var inputVal = $(this).val();
+			$.ajax({
+			   url: "https://en.wikipedia.org/w/api.php?action=query&titles="+inputVal+"&prop=revisions&rvprop=content&format=json",//"https://en.wikipedia.org/w/api.php?action=query&list=allcategories&acprefix=" + inputVal,
+	           type: "POST",
+	           dataType: "json",
+	           success: function(){
+			
+	           }
+			});
+		});
+	}  
+});
