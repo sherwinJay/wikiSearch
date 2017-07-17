324bde1fa67642a3aaecc0730d87e0a2c872e7b1
sherwinJay <sherwinsuva@gmail.com>
2017-07-18 04:48:42 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 			   header: {"Api-User-Agent" : "wikiSearch"},
 			   success: function(data){
 				//console.log("https://en.wikipedia.org/w/api.php?action=query&list=allcategories&acprefix=" + inputVal);
-				   console.log(lists.length);
+				   //console.log(lists.length);
 				   const lists = data.query.allcategories; 
 				   for(let i = 0; i <= lists.length; i++ ){
 					dataList += "<li class='listBg'>" + data.query.allcategories[i] + "</li>";
