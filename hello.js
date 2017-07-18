86a87b065628b1242115eb86a4084c0eea328058
sherwinJay <sherwinsuva@gmail.com>
2017-07-18 10:11:24 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -21,11 +21,11 @@ $(document).ready(function() {
 			   header: {"Api-User-Agent" : "wikiSearch"},
 			   success: function(data){
 				//console.log("https://en.wikipedia.org/w/api.php?action=query&list=allcategories&acprefix=" + inputVal);
-				   console.log(data.query.allcategories[0]["*"]);
+				   //console.log(data.query.allcategories[0]["*"]);
 				  const lists = data.query.allcategories; 
-				  /** for(let i = 0; i <= lists.length; i++ ){
-					dataList += "<li class='listBg'>" + data.query.allcategories[i]["*"] + "</li>";
-				   }**/
+				  for(let i = 0; i <= lists.length; i++ ){
+					dataList += "<li class='listBg'>" + data.query.allcategories[0][i]["*"] + "</li>";
+				   }
 				   dataList += "</ul>";
 				   document.getElementById("formWrapper").innerHTML = dataList;      		 }
 			});
