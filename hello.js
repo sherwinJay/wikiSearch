7d2c40c7c242183788e25bb4f9c1a2e9bc186208
sherwinJay <sherwinsuva@gmail.com>
2017-07-20 03:52:01 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 			   dataType: "jsonp",
 			   header: {"Api-User-Agent" : "wikiSearch"},
 			   success: function(data){
-				console.log("https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=" + inputVal);
+				console.log("https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrprop=snippet&gsrsearch=" + inputVal);
 				//console.log(data.query.pages.hasOwnProperty("title"));   
 				 const lists = data.query.pages; 
 				 /**for(let i = 0; i <= lists.length; i++ ){
