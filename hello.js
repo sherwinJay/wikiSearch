944089abb3944e667929bc291062fdc6ab52a62a
sherwinJay <sherwinsuva@gmail.com>
2017-07-20 03:40:27 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -16,14 +16,25 @@ $(document).ready(function() {
 			   success: function(data){
 				console.log("https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=" + inputVal);
 				//console.log(data.query.pages.hasOwnProperty("title"));   
-				 // const lists = data.query.search; 
+				 const lists = data.query.pages; 
 				 /**for(let i = 0; i <= lists.length; i++ ){
 					//dataList += "<li class='listBg'>" + data.query.allcategories[0]["*"] + "</li>";
 					console.log(data.query.allcategories[i]["*"]);
 				   }**/
-				  /** lists.forEach(function(item){
-				   	dataList += "<li class='listBg'>" + item.snippet + "</li>";
-				   });**/
+				var arr = [];
+				var i;
+				var count = 0;
+				for(i in a["test"]){
+				  if(query.pages.hasOwnProperty(i)){
+				    arr.push(i);
+				    count++;
+				  }
+				}
+				   lists.forEach(function(item){
+					for(var j = 0; j < count; j++){
+				   		dataList += "<li class='listBg'>" + item[arr[j]].title + "</li>";
+					}
+				   });
 				   dataList += "</ul>";
 				   document.getElementById("formWrapper").innerHTML = dataList;      		 }
 			});
