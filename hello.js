3438ad562d50d306081f2f7cbb43fd989e54925f
sherwinJay <sherwinsuva@gmail.com>
2017-07-21 05:57:40 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -32,11 +32,15 @@ $(document).ready(function() {
 				}**/
 				  //console.log(data.query.pages.extract);
 				  lists.forEach(function(item){
+					  //find if thumbnail property exists within the pages array of obj
 					  if(item.thumbnail == null){
+						  // if not found try to return or create a thumbnail.source obj
 						  console.log("Not Found");
+						  return false;
+						  
 					     }
 					  //console.log(item.thumbnail.source);
-					dataList += "<a href='" + "https://en.wikipedia.org/?curid=" + item.pageid + "' target='_blank'>" + "<li class='listBg'>" + item.title + "<p>" + item.extract + "</p>" + "</li>" + "</a>";
+					dataList += "<a href='" + "https://en.wikipedia.org/?curid=" + item.pageid + "' target='_blank'>" + "<li class='listBg'>" + "<img src='" + item.thumbnail.source + "'>" + item.title + "<p>" + item.extract + "</p>" + "</li>" + "</a>";
 					/**for(var j = 0; j < count; j++){
 				   		dataList += "<li class='listBg'>" + data.query.pages[arr[j]].title + "<p>" + data.query.pages[arr[j]].snippet + "</li>";
 					}**/
