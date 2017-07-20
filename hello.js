91f8cd3dded36ab5349702ec7021071e308a53b9
sherwinJay <sherwinsuva@gmail.com>
2017-07-21 03:31:38 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 			inputVal= encodeURIComponent(inputVal.trim());
 			$.ajax({
 			//"https://en.wikipedia.org/w/api.php?action=query&list=search&srprop=snippet&srsearch=intitle:" + inputVal + "&format=json"
-			   url: "https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=" + inputVal + "&format=json",
+			   url: "https://en.wikipedia.org/w/api.php?action=query&formatversion=2&generator=search&gsrsearch=" + inputVal + "&format=json",
 			   type: "POST",
 			   dataType: "jsonp",
 			   header: {"Api-User-Agent" : "wikiSearch"},
@@ -24,17 +24,18 @@ $(document).ready(function() {
 				var arr = [];
 				var i;
 				var count = 0;
-				for(i in data.query.pages){
+				/**for(i in data.query.pages){
 				  if(data.query.pages.hasOwnProperty(i)){
 				    arr.push(i);
 				    count++;
 				  }
-				}
-				  // lists.forEach(function(item){
-					for(var j = 0; j < count; j++){
+				}**/
+				  lists.forEach(function(item){
+					dataList += "<li class='listBg'>" + item.title + "<p>" + item.extract + "</li>";
+					/**for(var j = 0; j < count; j++){
 				   		dataList += "<li class='listBg'>" + data.query.pages[arr[j]].title + "<p>" + data.query.pages[arr[j]].snippet + "</li>";
-					}
-				 //  });
+					}**/
+				  });
 				   dataList += "</ul>";
 				   document.getElementById("formWrapper").innerHTML = dataList;      		 }
 			});
