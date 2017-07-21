2ed0e37b94266168a6686f089f2d3521e59825f6
sherwinJay <sherwinsuva@gmail.com>
2017-07-22 01:53:44 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("form").on("keyup", "input", function(){
 		var inputVal = $(this).val();
 		inputVal= encodeURIComponent(inputVal.trim());
-
+		dynamicSearch(inputVal);
 	});
 });
 function dynamicSearch(inputText){
