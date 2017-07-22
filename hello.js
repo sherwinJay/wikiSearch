896be8fc2333cea7f13446e82b8a0d9b61915654
sherwinJay <sherwinsuva@gmail.com>
2017-07-22 09:27:53 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -28,30 +28,20 @@ function dynamicSearch(inputText){
 			return false;
 			}
 		        let lists =  data.query.pages;
-		   	/*for(var i =0; i < data.query.pages.length; i++){
-				if( data.query.pages.length > 0 ){
-				if(data.query.pages[i].thumbnail == null){
+		   	for(var i =0; i < data.query.pages.length; i++){
+				if( data.query == null ){
 					return false;
+				}else{
+				if(data.query.pages[i].thumbnail == null){
 					//add objects
 					  data.query.pages[i].thumbnail = "source";
 					  data.query.pages[i].thumbnail.source = "";
-				     }**/
-				lists.forEach(function(item){				
-			  //find if thumbnail property exists within the pages array of obj
-				if(item.thumbnail == null){
-					//add objects
-					 // item.thumbnail = "source";
-					  //item.thumbnail.source = "";
-					return false;
 				     }
-	       dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + item.pageid + "' target='_blank'>"
-					  + "<img src='" + item.thumbnail.source + "'>" + "<h3>" + item.title + "</h3>" + "<p>" + item.extract + "</p>"
-					  + "</a>" + "</li>";
-			/**dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + data.query.pages[i].pageid + "' target='_blank'>"
+				
+			dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + data.query.pages[i].pageid + "' target='_blank'>"
 					  + "<img src='" + data.query.pages[i].thumbnail.source + "'>" + "<h3>" + data.query.pages[i].title + "</h3>" + "<p>" + data.query.pages[i].extract + "</p>"
-					  + "</a>" + "</li>";**/
-			
-			});
+					  + "</a>" + "</li>";
+				}
 				/**lists.forEach(function(item){
 				if(item != "" ){
 				  //find if thumbnail property exists within the pages array of obj
