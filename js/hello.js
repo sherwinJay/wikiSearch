66e1f898e2f23c48fbd0bdf797948ca74158c38e
sherwinJay <sherwinsuva@gmail.com>
2017-07-23 17:01:58 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -41,7 +41,7 @@ function dynamicSearch(inputText){
 				console.log(lists);
 				for(var i =0; i < lists.length; i++)
 				{
-					if(lists[i].thumbnail == null)
+					if(lists[i].thumbnail.source == null)
 					{
 						//add objects
 						//lists[i].thumbnail = "source";
