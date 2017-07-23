d0c8c65410ee6afaf09e1fe8db01c0d9b4564c1d
sherwinJay <sherwinsuva@gmail.com>
2017-07-23 12:25:02 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -12,11 +12,24 @@ function dynamicSearch(inputText){
 	$.ajax({
 		//"https://en.wikipedia.org/w/api.php?action=query&formatversion=2&generator=prefixsearch&gpssearch=" + inputText + "&prop=pageimages|extracts&piprop=thumbnail&pithumbsize=120&redirects=&exintro=true&exsentences=2&explaintext=true&rvprop=timestamp&format=json"
 	   url: "https://en.wikipedia.org/w/api.php?",
-	   data: {action: "query", formatversion: 2, generator: "prefixsearch", gpssearch: inputText, prop: "pageimages|extracts", piprop: "thumbnails", pithumbsize: 120,  exintro: true, exsentences: 2, explaintext: true, format: "json"},
-	  dataType: "jsonp",
-	  type: "POST", 
+	   data: {
+	   		action: "query", 
+	   		formatversion: 2, 
+	   		generator: "prefixsearch", 
+	   		gpssearch: inputText, 
+	   		prop: "pageimages|extracts", 
+	   		piprop: "thumbnails", 
+	   		pithumbsize: 120, 
+	   		redirects: "", 
+	   		exintro: true, 
+	   		exsentences: 2, 
+	   		explaintext: true, 
+	   		format: "json"
+	   	},
+	   type: "POST",
+	   dataType: "jsonp",
 	   header: {"Api-User-Agent" : "wikiSearch"},
-	   success: function(data){
+	   success: function(result){
 		//console.log("https://en.wikipedia.org/w/api.php?action=query&formatversion=2&generator=prefixsearch&gpssearch=" + inputText + "&prop=pageimages|extracts&piprop=thumbnail&pithumbsize=120&redirects=&exintro=true&exsentences=2&explaintext=true&rvprop=timestamp");
 		//console.log(inputText.length);
 		const inputLength = 0;
@@ -25,24 +38,24 @@ function dynamicSearch(inputText){
 			var dataList = "<ul>";
 			var formContainer = document.getElementById("formWrapper");
 			// try for loop
-		   	if( data.query == null ){
+		   	if( result.query == null ){
 				//dataList += "";
 					return dynamicSearch();
 				}
-		        let lists =  data.query.pages;
-		   	/**for(var i =0; i < data.query.pages.length; i++){
-				if( data.query == null ){
+		        let lists =  result.query.pages;
+		   	for(var i =0; i < result.query.pages.length; i++){
+				if( result.query == null ){
 					return false;
-				}else{**/
-				if(data.query.pages[i].thumbnail == null){
+				}else{
+				if(result.query.pages[i].thumbnail == null){
 					//add objects
-					  data.query.pages[i].thumbnail = "source";
-					  data.query.pages[i].thumbnail.source = "";
+					  result.query.pages[i].thumbnail = "source";
+					  result.query.pages[i].thumbnail.source = "";
 				     }
 				
-			dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + data.query.pages[i].pageid + "' target='_blank'>"
-					  + "<img src='" + data.query.pages[i].thumbnail.source + "'>" + "<h3>" + data.query.pages[i].title + "</h3>" + "<p>" 
-					  + data.query.pages[i].extract + "</p>" + "</a>" + "</li>";
+			dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + result.query.pages[i].pageid + "' target='_blank'>"
+					  + "<img src='" + result.query.pages[i].thumbnail.source + "'>" + "<h3>" + result.query.pages[i].title + "</h3>" + "<p>" 
+					  + result.query.pages[i].extract + "</p>" + "</a>" + "</li>";
 				}
 				/**lists.forEach(function(item){
 				if(item != "" ){
@@ -59,7 +72,7 @@ function dynamicSearch(inputText){
 				dataList += "";
 					}
 				});**/
-			//}
+			}
 		   dataList += "</ul>";
 		   	document.getElementById("formWrapper").innerHTML = dataList;
 	  		
@@ -67,4 +80,3 @@ function dynamicSearch(inputText){
 	});
 
 }  
-
