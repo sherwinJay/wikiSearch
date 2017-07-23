b25b45874ec32dd95b2426f939b2bd65007bb450
sherwinJay <sherwinsuva@gmail.com>
2017-07-24 07:42:06 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -42,7 +42,7 @@ function dynamicSearch(inputText){
 				for(var i =0; i < lists.length; i++)
 				{console.log(lists[i].thumbnail);
 					for( key in lists.thumbnail ){
-						if(lists[i].thumbnail == undefined ){
+						if(lists[key].thumbnail == undefined ){
 							console.log("true");
 						}
 					}    
