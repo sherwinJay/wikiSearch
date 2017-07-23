166cbaf26cfa3586871badbc5bbc523a7974af4a
sherwinJay <sherwinsuva@gmail.com>
2017-07-24 07:36:01 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -40,7 +40,7 @@ function dynamicSearch(inputText){
 				let lists =  result.query.pages;
 				console.log(lists);
 				for(var i =0; i < lists.length; i++)
-				{
+				{console.log(lists[i].thumbnail);
 					for( key in lists.thumbnail ){
 						if(lists[i].thumbnail == "" ){
 							console.log("true");
