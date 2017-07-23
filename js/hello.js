462f119df1be45277787b28c43d8c80a8e9ffcd5
sherwinJay <sherwinsuva@gmail.com>
2017-07-23 16:31:50 +0800

    Add files via upload

---
+++ js/hello.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+	$("form").on("keyup", "input", function(e){
+		e.preventDefault();
+		var inputVal = $(this).val();
+		inputVal= encodeURIComponent(inputVal.trim());
+		dynamicSearch(inputVal);
+	});
+});
+function dynamicSearch(inputText){
+	var $url = "https://en.wikipedia.org/w/api.php?";
+	$.ajax({//"https://en.wikipedia.org/w/api.php?action=query&formatversion=2&generator=prefixsearch&gpssearch=" + inputText + "&prop=pageimages|extracts&piprop=thumbnail&pithumbsize=120&redirects=&exintro=true&exsentences=2&explaintext=true&rvprop=timestamp&format=json"
+	   url: $url,
+	   data: {
+		action: "query", 
+		formatversion: 2, 
+		generator: "prefixsearch", 
+		gpssearch: inputText, 
+		prop: "pageimages|extracts", 
+		piprop: "thumbnail", 
+		pithumbsize: 120, 
+		exintro: "true", 
+		exsentences: 2, 
+		explaintext: "true",
+		format: "json"
+	   	},
+	   dataType: "jsonp",	
+	   type: "POST",
+	   header: {"Api-User-Agent" : "wikiSearch"},
+	   success: function(result)
+	   {
+			var dataList = "<ul>";
+			var formContainer = document.getElementById("formWrapper");
+		   	if( result.query == null || result.continue == null )
+			{
+				console.log("true");
+				result.errors = "Nothing Found";
+				console.log(result.errors);
+				dataList += "<li>" + result.errors + "</li>";
+			}
+			else
+			{
+				let lists =  result.query.pages;
+				console.log(lists);
+				for(var i =0; i < lists.length; i++)
+				{
+					if(lists[i].thumbnail == null)
+					{
+						//add objects
+						lists[i].thumbnail = "source";
+						lists[i].thumbnail.source = "<img src='images/no-thumbnail.jpg'>";
+					}
+					dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + lists[i].pageid + "' target='_blank'>"
+					+ "<img src='" + lists[i].thumbnail.source + "'>" + "<h3>" + lists[i].title + "</h3>"
+					+ "<p>" + lists[i].extract + "</p>" + "</a>" + "</li>";
+				}
+			}
+		  	dataList += "</ul>";
+		   	document.getElementById("formWrapper").innerHTML = dataList;	  		
+   		}	 
+	});
+}
