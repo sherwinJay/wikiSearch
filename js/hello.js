e10975afe191e99e04d159a31aadbf0a5c260d12
sherwinJay <sherwinsuva@gmail.com>
2017-07-24 08:18:30 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -30,7 +30,7 @@ function dynamicSearch(inputText){
 	   {console.log("https://en.wikipedia.org/w/api.php?action=query&formatversion=2&generator=prefixsearch&gpssearch=" + inputText + "&prop=pageimages|extracts&piprop=thumbnail&pithumbsize=120&redirects=&exintro=true&exsentences=2&explaintext=true");
 			var dataList = "<ul>";
 			var formContainer = document.getElementById("formWrapper");
-		   	if( result.query == null || result.continue == null )
+		   	if( result.query == null )
 			{
 				result.errors = "Nothing Found";
 				dataList += "<li>" + result.errors + "</li>";
@@ -38,29 +38,18 @@ function dynamicSearch(inputText){
 			else
 			{
 				let lists =  result.query.pages;
-				console.log(lists);
 				for(var i =0; i < lists.length; i++)
-				{console.log(lists[i].thumbnail.source);
+				{
 					for( key in lists ){
 						if(lists[key].thumbnail == undefined ){
 							console.log("true");
 							lists[key].thumbnail = {};
 							lists[key].thumbnail.source = "images/no-thumbnail.jpg";
 						}
-					}    
-					/**if(lists[i].thumbnail == null)
-					{
-					lists[i].thumbnail = "source";	
-					lists[i].thumbnail.source = "HI";
-						console.log(lists[i].thumbnail);
-						if(lists[i].thumbnail.source === "undefined"){
-						lists[i].thumbnail.source = "images/no-thumbnail.jpg";
-						console.log(lists[i].thumbnail.source);	
-						}
-					}**/
-					dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + lists[i].pageid + "' target='_blank'>"
-					+ "<img src='" + lists[i].thumbnail.source + "'>" + "<h3>" + lists[i].title + "</h3>"
-					+ "<p>" + lists[i].extract + "</p>" + "</a>" + "</li>";
+					}   
+				dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + lists[i].pageid + "' target='_blank'>"
+				+ "<img src='" + lists[i].thumbnail.source + "'>" + "<h3>" + lists[i].title + "</h3>"
+				+ "<p>" + lists[i].extract + "</p>" + "</a>" + "</li>";
 					
 				}
 			}
