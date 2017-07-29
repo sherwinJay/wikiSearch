7fece301819f8eb528c341a5331cd6d1c97d8a08
sherwinJay <sherwinsuva@gmail.com>
2017-07-29 18:59:00 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -87,7 +87,7 @@ function submitBtn(){
 		e.preventDefault();
 		dynamicSearch();
 		var $mainContainer = $(this).closest(".mainContainer");
-		$mainContainer.find(".centerLayout").removeClass("centerLayout");
+		$mainContainer.find(".centerLayout").css({"display": "none"});
 		//$mainContainer.find(".formContainer").addClass("topLayout");
 		
 	});
