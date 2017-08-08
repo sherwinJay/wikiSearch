e8a35155a0a2f7de4e144462cdec3fd93aec07b8
sherwinJay <sherwinsuva@gmail.com>
2017-08-09 04:49:55 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -59,6 +59,8 @@ console.log("https://en.wikipedia.org/w/api.php?action=query&formatversion=2&gen
 					} **/  
 					if(lists[i].thumbnail == undefined){
 					console.log("TRUE");
+					lists[i].thumbnail = {};
+					lists[i].thumbnail.source = "images/no-thumbnail.jpg";
 					}
 				dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + lists[i].pageid + "' target='_blank'>"
 				+ "<div class='imgContainer'>" +  "<img src='" + lists[i].thumbnail.source + "'>" + "</div>" + "<div class='snippet-container'>" 
