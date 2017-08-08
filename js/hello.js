2811c07ba8c582ff0746e60e42acd01b479fa19d
sherwinJay <sherwinsuva@gmail.com>
2017-08-09 05:07:59 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -57,11 +57,6 @@ console.log("https://en.wikipedia.org/w/api.php?action=query&formatversion=2&gen
 							lists[key].thumbnail.source = "images/no-thumbnail.jpg";
 						}
 					} 
-					/**if( lists[i].hasOwnProperty("thumbnail")  == false ){
-					console.log("TRUE");
-					lists[i].thumbnail = {};
-					lists[i].thumbnail.source = "images/no-thumbnail.jpg";
-					}**/
 				dataList += "<li class='listBg'>"  + "<a href='" + "https://en.wikipedia.org/?curid=" + lists[i].pageid + "' target='_blank'>"
 				+ "<div class='imgContainer'>" +  "<img src='" + lists[i].thumbnail.source + "'>" + "</div>" + "<div class='snippet-container'>" 
 				+ "<h3>" + lists[i].title + "</h3>" + "<p>" + lists[i].extract + "</p>" + "</div>" + "</a>" + "</li>";	
